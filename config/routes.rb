95f0450c0fa47a94cb956c7b462ec7cdf3a51078
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-23 17:04:32 +0300

    Add view file for react component

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "static#index"
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
-  # Defines the root path route ("/")
-  root "static#index"
 end
