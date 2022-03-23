b403ca4c826a794ea2fb64dc38b614fd7a92dcc6
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-23 18:44:35 +0300

    Update App.js file

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,19 +1,25 @@
 import React from "react"
-import PropTypes from "prop-types"
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import { Provider } from "react-redux"
+
 import HelloWorld from './HelloWorld'
+
+import configureStore from "../configureStore"
+const store = configureStore();
 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" render={() => ("Home!")} />
-          <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>}/>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" render={() => ("Home!")} />
+            <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>}/>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
