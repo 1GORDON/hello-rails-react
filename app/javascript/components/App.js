f34c23c15b4b08736d465affc2279de952e8f246
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-23 17:46:35 +0300

    Update the App component with Routes instead of Switch

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HelloWorld from './HelloWorld'
 
@@ -9,10 +9,10 @@ class App extends React.Component {
   render () {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() => ("Home")} />
+        <Routes>
+          <Route exact path="/" render={() => ("Home!")} />
           <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>}/>
-        </Switch>
+        </Routes>
       </BrowserRouter>
     );
   }
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
 end
