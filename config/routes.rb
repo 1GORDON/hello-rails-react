ff9dde64fd36221297cba8afa4cb3115c6e788c8
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-23 16:15:46 +0300

    Add GET/v1/things endpoint to routes and things_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :v1, defaults: { format: 'json' } do
+    get 'things', to: 'things#index'
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
