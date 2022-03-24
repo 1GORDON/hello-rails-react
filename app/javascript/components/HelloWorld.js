43318f9da30cb206e6602aef694836d0981b34ec
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-24 09:44:05 +0300

    Add a ul list to the HelloWorld to fill it with things from the redux state

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { connect } from 'react-redux';
-import { createStructuredSelector, createStructuredSelector } from 'reselect';
+import { createStructuredSelector } from 'reselect';
 
 const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST';
 
 function getThings(){
-  console.log('getThings() Action!!!')
+  console.log('getThings() Action!!')
   return {
     type: GET_THINGS_REQUEST
   };
@@ -13,19 +13,25 @@ function getThings(){
 
 class HelloWorld extends React.Component {
   render () {
+    const { things } = this.props;
+    const thingsList = things.map((thing) => {
+      return <li>{thing.name} {thing.guid}</li>
+    })
     return (
       <React.Fragment>
         Greeting: {this.props.greeting}
         <button className="getThingsBtn" onClick={() => this.props.getThings()}>getThings</button>
+        <br />
+        <ul>{ thingsList }</ul>
       </React.Fragment>
     );
   }
 }
 
-const createStructuredSelector = createStructuredSelector({
+const structuredSelector = createStructuredSelector({
   things: state => state.things,
 });
 
 const mapDispatchToProps = { getThings };
 
-export default connect(createStructuredSelector, mapDispatchToProps)(HelloWorld);
+export default connect(structuredSelector, mapDispatchToProps)(HelloWorld);
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root "static#index"
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
   get '*page', to: 'static#index', constraints: ->(req) do
     !req.xhr? && req.format.html?
   end
+  root "static#index"
 end
